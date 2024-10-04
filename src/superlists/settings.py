2576ace919781758ad1885551487412e0ab67b0b
Isaac Robinson <69752141+IJRCreate@users.noreply.github.com>
2024-10-04 23:57:10 +0100

    Added collecstatic to dockerfile, and new location to gitignore.

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-v8o@clb008#f#)w#x5l80#no2unm&95nmot+*ve(v%!**lw#u&"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+if "DJANGO_DEBUG_FALSE" in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+    ALLOWED_HOSTS = [os.environ["DJANGO_ALLOWED_HOST"]]
+else:
+    DEBUG = True
+    SECRET_KEY = "django-insecure-v8o@clb008#f#)w#x5l80#no2unm&95nmot+*ve(v%!**lw#u&"
+    ALLOWED_HOSTS = []
 
 
 # Application definition
