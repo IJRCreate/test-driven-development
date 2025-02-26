ee9b9d13f87c91d7fffc1a07ae69e1c8d58593bc
Isaac Robinson <69752141+IJRCreate@users.noreply.github.com>
2025-02-26 01:09:28 +0000

    Use get_absolute_url on List model to DRY urls in views

--- src/lists/tests/test_models.py
+++ src/lists/tests/test_models.py
@@ -43,3 +43,7 @@ class ListAndItemModelsTest(TestCase):
         item = Item(list=mylist, text="")
         with self.assertRaises(ValidationError):
             item.full_clean()  # Django models don’t run full validation on save.
+    
+    def test_get_absolute_url(self):
+        mylist = List.objects.create()
+        self.assertEqual(mylist.get_absolute_url(), f"/lists/{mylist.id}/")
