2a9ed0e59b04629a05c00e10ec3dd3c727add23b
Isaac Robinson <69752141+IJRCreate@users.noreply.github.com>
2025-03-11 22:10:05 +0000

    Added ability to capture errors. Deliberately broke functional test.

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -6,9 +6,12 @@ from selenium.webdriver.common.keys import Keys
 from unittest import skip
 import os
 import time
+from datetime import datetime
+from pathlib import Path
 
 MAX_WAIT = 5
 
+SCREEN_DUMP_LOCATION = Path(__file__).absolute().parent / "screendumps"
 
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
@@ -18,7 +21,33 @@ class FunctionalTest(StaticLiveServerTestCase):
             self.live_server_url = "http://" + test_server
 
     def tearDown(self):
+        if self._test_has_failed():
+            if not SCREEN_DUMP_LOCATION.exists():
+                SCREEN_DUMP_LOCATION.mkdir(parents=True)
+            self.take_screenshot()
+            self.dump_html()
         self.browser.quit()
+        super().tearDown()
+
+    def _test_has_failed(self):
+        # slightly obscure but couldn't find a better way!
+        return self._outcome.result.failures or self._outcome.result.errors
+
+    def take_screenshot(self):
+            path = SCREEN_DUMP_LOCATION / self._get_filename("png")
+            print("screenshotting to", path)
+            self.browser.get_screenshot_as_file(str(path))
+
+    def dump_html(self):
+        path = SCREEN_DUMP_LOCATION / self._get_filename("html")
+        print("dumping page HTML to", path)
+        path.write_text(self.browser.page_source)
+
+    def _get_filename(self, extension):
+            timestamp = datetime.now().isoformat().replace(":", ".")[:19]
+            return (
+                f"{self.__class__.__name__}.{self._testMethodName}-{timestamp}.{extension}"
+            )
 
     def wait_for(self, fn):
         start_time = time.time()
